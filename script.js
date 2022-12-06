414d76c4692214bb9a61b645688a224d5f343144
Vanessa Neves <113128675+vanessaneves-dev@users.noreply.github.com>
2022-12-06 12:51:38 -0300

    :sparkles: feat: add interações

--- script.js
+++ script.js
@@ -1,19 +1,76 @@
-const tabMenu = document.querySelectorAll('.js-tabmenu li');
-const tabContent = document.querySelectorAll('.js-tabcontent section');
+function inicActiveTab() {
+  const tabMenu = document.querySelectorAll('.js-tabmenu li');
+  const tabContent = document.querySelectorAll('.js-tabcontent section');
 
-if (tabContent.length && tabMenu.length) {
-  tabContent[0].classList.add('ativo');
+  if (tabContent.length && tabMenu.length) {
+    tabContent[0].classList.add('ativo');
 
-  function activeTab(index) {
-    tabContent.forEach((content) => {
-      content.classList.remove('ativo');
+    function activeTab(index) {
+      tabContent.forEach((content) => {
+        content.classList.remove('ativo');
+      });
+      tabContent[index].classList.add('ativo');
+    }
+
+    tabMenu.forEach((itemMenu, index) => {
+      itemMenu.addEventListener('click', () => {
+        activeTab(index);
+      });
     });
-    tabContent[index].classList.add('ativo');
+  }
+}
+
+inicActiveTab();
+
+
+function inicAccordion() {
+  const accordionList = document.querySelectorAll('.js-accordion dt');
+  const activeClass = 'ativo'
+  if (accordionList.length) {
+    accordionList[0].classList.add('activeClass');
   }
 
-  tabMenu.forEach((itemMenu, index) => {
-    itemMenu.addEventListener('click', () => {
-      activeTab(index);
-    });
+  accordionList[0].classList.add('activeClass');
+  accordionList[0].nextElementSibling.classList.add('activeClass');
+
+
+
+  function activeAccordion() {
+    this.classList.toggle('activeClass');
+    this.nextElementSibling.classList.toggle('activeClass');
+  }
+
+  accordionList.forEach((item) => {
+    item.addEventListener('click', activeAccordion);
   });
 }
+inicAccordion();
+
+
+
+
+function initScrollSuave() {
+  const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
+
+  function scrollToSection(event) {
+    event.preventDefault();
+    const href = event.currentTarget.getAttribute('href');
+    const section = document.querySelector(href);
+    section.scrollIntoView({
+      behavior: 'smooth',
+      block: 'start',
+    });
+
+    // forma alternativa
+    // const topo = section.offsetTop;
+    // window.scrollTo({
+    //   top: topo,
+    //   behavior: 'smooth',
+    // });
+  }
+
+  linksInternos.forEach((link) => {
+    link.addEventListener('click', scrollToSection);
+  });
+}
+initScrollSuave();
