8eb107d7f05c6eb36a0c54a2a344e1a56ec5bc71
Vanessa Neves <113128675+vanessaneves-dev@users.noreply.github.com>
2022-12-07 16:37:37 -0300

    :recycle: refactor: organização do js com modules

---
+++ js/modules/nav-tab.js
@@ -0,0 +1,21 @@
+export default function initActiveTab() {
+  const tabMenu = document.querySelectorAll('[data-tab="menu"] li');
+  const tabContent = document.querySelectorAll('[data-tab="content"] section');
+
+  if (tabContent.length && tabMenu.length) {
+    tabContent[0].classList.add('ativo');
+
+    function activeTab(index) {
+      tabContent.forEach((content) => {
+        content.classList.remove('ativo');
+      });
+      tabContent[index].classList.add('ativo');
+    }
+
+    tabMenu.forEach((itemMenu, index) => {
+      itemMenu.addEventListener('click', () => {
+        activeTab(index);
+      });
+    });
+  }
+}
---
+++ js/script.js
@@ -0,0 +1,11 @@
+import initScrollSuave from "./modules/scroll-suave.js";
+import initAnimacaoScroll from "./modules/scroll-animacao.js";
+import initActiveTab from "./modules/nav-tab.js";
+import initAccordion from "./modules/accordion.js";
+
+
+
+initScrollSuave();
+initAnimacaoScroll();
+initAccordion();
+initActiveTab();
