7dc34529b7aa894b65a942697b0876cb639c3efb
Vanessa Neves <113128675+vanessaneves-dev@users.noreply.github.com>
2022-12-07 18:17:35 -0300

    :dizzy: feat: add Modal

--- js/modules/scroll-animacao.js
+++ js/modules/scroll-animacao.js
@@ -19,3 +19,5 @@ export default function initAnimacaoScroll() {
     window.addEventListener('scroll', animaScroll);
   }
 }
+
+
--- js/script.js
+++ js/script.js
@@ -2,6 +2,8 @@ import initScrollSuave from "./modules/scroll-suave.js";
 import initAnimacaoScroll from "./modules/scroll-animacao.js";
 import initActiveTab from "./modules/nav-tab.js";
 import initAccordion from "./modules/accordion.js";
+import initModal from "./modules/modal.js";
+
 
 
 
@@ -9,3 +11,4 @@ initScrollSuave();
 initAnimacaoScroll();
 initAccordion();
 initActiveTab();
+initModal();
