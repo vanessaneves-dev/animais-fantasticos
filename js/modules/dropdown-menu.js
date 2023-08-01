41e28c61e3108e18ee480c8da96c45f58ea53b63
vanessaneves-dev <vanessaneves.dev@gmail.com>
2023-08-01 16:51:28 -0300

    :zap:perf melhoria de performace de events

--- js/modules/dropdown-menu.js
+++ js/modules/dropdown-menu.js
@@ -1,30 +1,35 @@
-export default function initDropdownMenu() {
-  
-}
-
-const dropdownMenus = document.querySelectorAll('[data-dropdown]');
-
-dropdownMenus.forEach(menu => {
-    ['touchstart', 'click'].forEach(userEvent => { 
-    menu.addEventListener(userEvent, handleClick);
- 
-})
-});
-
-function handleClick(event) {
-    event.preventDefault();
-    this.classList.add('active');
-    outsideClick(this, () => {
-        this.classList.remove('active');
+export default function initDropdownMenu() {  
+    
+    const dropdownMenus = document.querySelectorAll('[data-dropdown]');
+    
+    dropdownMenus.forEach(menu => {
+        ['touchstart', 'click'].forEach(userEvent => { 
+            menu.addEventListener(userEvent, handleClick);
+            
+        })
     });
-}
-function outsideClick(element, callback) {
-    const html = document.documentElement;
-    html.addEventListener('click', handleOutsideClick);
-    function handleOutsideClick(event) {
-        if(!element.contains(event.target)) {
-            html.removeEventListener('click', handleOutsideClick);
-            callback();
+    
+    function handleClick(event) {
+        event.preventDefault();
+        this.classList.add('active');
+        outsideClick(this, () => {
+            this.classList.remove('active');
+        });
+    }
+    function outsideClick(element, callback) {
+        const html = document.documentElement;
+        const outside = 'data-outside';
+
+        if(!element.hasAttribute(outside)) {
+        html.addEventListener('click', handleOutsideClick);
+        element.setAttribute('outside', '');
+        }
+        function handleOutsideClick(event) {
+            if(!element.contains(event.target)) {
+                element.removeAttribute('outside');
+                html.removeEventListener('click', handleOutsideClick);
+                callback();
+            }
         }
     }
 }
