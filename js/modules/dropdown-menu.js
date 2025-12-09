fc87186f809e43403cb0fe63373c12cb092e4cb2
Vanessa Neves <113128675+vanessaneves-dev@users.noreply.github.com>
2025-12-09 16:34:25 -0300

    Improve accessibility and dropdown behavior

--- js/modules/dropdown-menu.js
+++ js/modules/dropdown-menu.js
@@ -1,22 +1,22 @@
 import outsideClick from "./outsideClick.js";
 
-export default function initDropdownMenu() {  
-    
-    const dropdownMenus = document.querySelectorAll('[data-dropdown]');
-    
-    dropdownMenus.forEach(menu => {
-        ['touchstart', 'click'].forEach(userEvent => { 
-            menu.addEventListener(userEvent, handleClick);
-            
-        })
+export default function initDropdownMenu() {
+  const dropdownMenus = document.querySelectorAll('[data-dropdown]');
+  const events = ['touchstart', 'click'];
+
+  if (dropdownMenus.length) {
+    dropdownMenus.forEach((menu) => {
+      events.forEach((userEvent) => {
+        menu.addEventListener(userEvent, handleClick);
+      });
     });
-    
-    function handleClick(event) {
-        event.preventDefault();
-        this.classList.add('active');
-        outsideClick(this, ['touchstart', 'click'], () => {
-            this.classList.remove('active');
-        });
-    }   
+  }
+
+  function handleClick(event) {
+    event.preventDefault();
+    this.classList.add('active');
+    outsideClick(this, events, () => {
+      this.classList.remove('active');
+    });
+  }
 }
-    
